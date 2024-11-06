4471b71c63182ac3fa6226473f9cd29f025a76a1
pkm021118 <pkm021117@gmail.com>
2024-11-06 15:14:13 +0900

    ProjectDetail 컴포넌트 생성

---
+++ front/src/components/ProjectDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectDetail = ({ project }) => {
+    return (
+        <div>
+            <h2>{project.name}</h2>
+            {/* 추가 정보 표시 */}
+        </div>
+    );
+};
+
+export default ProjectDetail;
