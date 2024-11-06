a98a588010107eb311140cafb776fc350a263e6a
pkm021118 <pkm021117@gmail.com>
2024-11-06 15:15:17 +0900

    라우트 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/ProjectPage';
+import TaskPage from './pages/TaskPage';
+import { ProjectProvider } from './context/ProjectContext';
+import { TaskProvider } from './context/TaskContext';
+
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    return (
+        <ProjectProvider>
+            <TaskProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/projects/:projectId" element={<TaskPage />} />
+                    </Routes>
+                </Router>
+            </TaskProvider>
+        </ProjectProvider>
+    );
 }
 
 export default App;
